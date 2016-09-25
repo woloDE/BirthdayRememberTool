4a95dee415b7b7feed1c9c9ddf8c12edcd1ce447
woloDe <kubed@web.de>
2016-09-25 13:02:58 +0200

    neu

--- src/logic/DaysToBirthday.java
+++ src/logic/DaysToBirthday.java
@@ -1,7 +1,17 @@
 package logic;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
 public class DaysToBirthday {
 	
+	public static int calculateDaysToBirthday(DateTime now, DateTime birthday){
 	
-
+		int daysToBirth;
+		System.out.println(Days.daysBetween(now, birthday));
+		daysToBirth = Days.daysBetween(now, birthday).getDays();
+		System.out.println(daysToBirth);
+		return daysToBirth;
+	}
+	
 }
--- src/logic/TestMain.java
+++ src/logic/TestMain.java
@@ -1,10 +1,20 @@
 package logic;
 
+import org.joda.time.DateTime;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		Birthday test = new Birthday(9, 12, 1992);
 		test.tellBirthday();
+		DateTime today = new DateTime(2016,12,9,1,1);
+		
+		DateTime myBirthday = new DateTime();
+		
+		DaysToBirthday.calculateDaysToBirthday(myBirthday,today );
+
+		
+		
 
 	}
 
